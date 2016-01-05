59c9209eace8fcbdede650457dc6079fc323e0de
zc1415926 <zc1415926@126.com>
2016-01-05 11:24:29 +0800

    remove some useless code

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -21,22 +21,5 @@ class CorsMiddleware
                 'GET, POST, PUT, DELETE, OPTIONS')
             ->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization, X-Request-With')
             ->header('Access-Control-Allow-Credentials', 'true');
-
-       /* $headers = [
-            'Access-Control-Allow-Origin' =>' *',
-            'Access-Control-Allow-Methods'=>' POST, GET, OPTIONS, PUT, DELETE',
-            'Access-Control-Allow-Headers'=>' Content-Type, Accept, Authorization, X-Requested-With'
-        ];
-
-        if($request->getMethod() == "OPTIONS") {
-            return \Response::make('OK', 200, $headers);
-        }
-
-        $response = $next($request);
-        foreach ($headers as $key => $value) {
-            $response->header($key, $value);
-        }
-
-        return $response;*/
     }
 }
